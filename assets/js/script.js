fb18e1e2fc2129437f8829c65f6661193804d0b0
kalyani kommula <kalyanib.tech505@gmail.com>
2024-05-24 13:16:03 +0000

    add list items in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 const start = document.getElementById("start-btn");
 start.addEventListener('click', startQuiz);
+const menu = document.getElementById("menu");
+const onlineQuiz = document.getElementById("onlineQuiz-container");
+
 
 function startQuiz() {
     console.log("start");
+    start.classList.add("hide");
+    menu.classList.add("hide");
+    onlineQuiz.classList.remove("hide");
+
 }
