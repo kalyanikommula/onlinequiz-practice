1530a65d4ee33139f2526d142c282ddb7de23c72
kalyani kommula <kalyanib.tech505@gmail.com>
2024-05-24 14:11:15 +0000

    create questions object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,37 @@
+const questions = [
+    {
+        question: "What is the England national Animal?",
+        answers: [
+            { image: "assets/images/leopard.png", correct: false},
+            { image: "assrts/images/Lion.png", correct: true},
+            {image: "assets/images/Horse.png", correct: false},
+        ]
+    },
+    {
+        question: "Which city is belongs to the England?",
+        answers: [
+            { image: "assets/images/london-bridge.png", correct: true},
+            { image: "assrts/images/eiffel-tower", correct: false},
+            {image: "assets/images/Tajmahal", correct: false},
+        ]
+    },
+    {
+        question: "Which one is the National Animal of India?",
+        answers: [
+            { image: "assets/images/deer.png", correct: false},
+            { image: "assrts/images/elephant.png", correct: false},
+            {image: "assets/images/Tiger.png", correct: true},
+        ]
+    },
+    {
+        question: "What is the national flower of India?",
+        answers: [
+            { image: "assets/images/Rose.png", correct: false},
+            { image: "assrts/images/lotus.png", correct: true},
+            {image: "assets/images/sunflower.png", correct: false},
+        ]
+    },
+]
 const start = document.getElementById("start-btn");
 start.addEventListener('click', startQuiz);
 const menu = document.getElementById("menu");
